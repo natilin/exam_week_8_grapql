88acc1fdefe5ad10f2b2143a75f3609a8b75359e
Nati Lintzer <nlinzer@gmail.com>
2024-10-31 10:52:56 +0200

    add mission_type.py

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -1,3 +1,9 @@
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
+
+from .countries import Countries
+from .cities import Cities
+from .missions import Missions
+from .targets import Targets
+from .targettypes import TargetTypes
---
+++ app/gql/types/mission_type.py
@@ -0,0 +1,15 @@
+from graphene import ObjectType, Int, String, Float, Date
+
+
+
+
+class MissionType(ObjectType):
+    mission_id = Int()
+    mission_date = String()
+    airborne_aircraft = Float()
+    attacking_aircraft = Float()
+    bombing_aircraft = Float()
+    aircraft_returned = Float()
+    aircraft_failed = Float()
+    aircraft_damaged = Float()
+    aircraft_lost = Float()
